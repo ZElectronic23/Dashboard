eac41345dfcd03266aa120a5b1fce8504e56f1fc
Z.Electronic <163954625+ZElectronic23@users.noreply.github.com>
2025-09-04 04:03:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuration file
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxIFHzm5K7bQr1Pat_jZwevUqnkz-cgtNnAd4z_lygzd78olctaeYshbeq1_zvA78-O/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycby6HGtTLZ2yQODxYdor9iK_aVNJwwNB6ZIZuCmQS9BF3uBNeVExM_FYlXpKd4qFLhBZ/exec"
 };
 
